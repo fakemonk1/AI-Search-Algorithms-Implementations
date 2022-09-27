d173b26391fafef113407cc273359ea7692bb60b
Thanush <thanushbabu460@gmail.com>
2022-09-27 15:49:43 +0530

    changed manhattan to geographical distance

--- Search_Algorithms.py
+++ Search_Algorithms.py
@@ -200,14 +200,14 @@ def astar_search(graph, start, goal):
         return path, explored_nodes
 
     path.append(start)
-    path_cost = get_manhattan_heuristic(start, goal)
+    path_cost = get_geographical_heuristic(start, goal)
     # Priority Queue to keep sorted distance travelled till now
     frontier = [(path_cost, path)]
     while len(frontier) > 0:
         # pop a node from the queue
         path_cost_till_now, path_till_now = pop_frontier(frontier)
         current_node = path_till_now[-1]
-        path_cost_till_now = path_cost_till_now - get_manhattan_heuristic(current_node, goal)
+        path_cost_till_now = path_cost_till_now - get_geographical_heuristic(current_node, goal)
         explored_nodes.append(current_node)
         # test goal condition
         if current_node == goal:
@@ -225,7 +225,7 @@ def astar_search(graph, start, goal):
 
             # extra_cost = graph.get_edge_weight(current_node, neighbour)
             extra_cost = 1
-            neighbour_cost = extra_cost + path_cost_till_now + get_manhattan_heuristic(neighbour, goal)
+            neighbour_cost = extra_cost + path_cost_till_now + get_geographical_heuristic(neighbour, goal)
             new_element = (neighbour_cost, path_to_neighbour)
 
             is_there, indexx, neighbour_old_cost, _ = get_frontier_params_new(neighbour, frontier)
@@ -293,6 +293,16 @@ def get_manhattan_heuristic(node, goal):
     manhattan_dist = i_delta + j_delta
     return manhattan_dist
 
+import math
+
+def get_geographical_heuristic(node,goal):
+    i, j = divmod(int(node), 8)
+    i_goal, j_goal = divmod(int(goal), 8)
+    i_delta = abs(i - i_goal)
+    j_delta = abs(j - j_goal)
+    
+    geographical_dist=math.sqrt((i_delta)**2+(j_delta)**2)
+    return geographical_dist
 
 if __name__ == '__main__':
     graph_neighbours = generate_graph()
@@ -305,7 +315,7 @@ if __name__ == '__main__':
     # print()
 
     print("============ AStar Search ================")
-    path_astar, explored_astar = astar_search(graph_neighbours, '0', '27')
+    path_astar, explored_astar = astar_search(graph_neighbours, '0', '61')
     print("Path_astar:", path_astar)
     print("Explored Nodes A Star: ", explored_astar)
     print(len(explored_astar))
